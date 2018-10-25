26bacabe8c342493ae9847747ba1ca35562832b0
NikiGreen <antimagantimagov@mail.ru>
2018-10-25 16:59:14 +0300

    SNAPSHOT 1.1

--- Arkanoid/Arkanoid/Source.cpp
+++ Arkanoid/Arkanoid/Source.cpp
@@ -6,25 +6,25 @@ int main()
 {
 	srand(time(0));
 
-	RenderWindow app(VideoMode(520, 450), "Arkanoid!");
+	RenderWindow app(VideoMode(520, 450), "Arkanoid ver 1.1");
 	app.setFramerateLimit(60);
 
-	Texture t1, t2, t3, t4;
-	t1.loadFromFile("images/block01.png");
-	t2.loadFromFile("images/background2.jpg");
-	t3.loadFromFile("images/ball.png");
-	t4.loadFromFile("images/paddle2.png");
+	Texture sp1, sp2, sp3, sp4;
+	sp1.loadFromFile("images/block01.png");
+	sp2.loadFromFile("images/background2.jpg");
+	sp3.loadFromFile("images/ball.png");
+	sp4.loadFromFile("images/paddle2.png");
 
-	Sprite sBackground(t2), sBall(t3), sPaddle(t4);
-	sPaddle.setPosition(300, 440);
+	Sprite sBackground(sp2), sBall(sp3), sPaddle(sp4);
+	sPaddle.setPosition(260, 440);
 
-	Sprite block[1000];
+	Sprite block[1000];//создание массива спрайтов
 
-	int n = 0;
+	int n = 0;//Придание элементам массива спрайтов загруженной текстуры блока
 	for (int i = 1; i <= 10; i++)
 		for (int j = 1; j <= 10; j++)
 		{
-			block[n].setTexture(t1);
+			block[n].setTexture(sp1);
 			block[n].setPosition(i * 43, j * 20);
 			n++;
 		}
@@ -32,15 +32,15 @@ int main()
 	float dx = 6, dy = 5;
 	float x = 300, y = 300;
 
-	while (app.isOpen())
+	while (app.isOpen())//При открытии окна
 	{
 		Event e;
-		while (app.pollEvent(e))
+		while (app.pollEvent(e))//Для закрытия программы при закрытии окна
 		{
 			if (e.type == Event::Closed)
 				app.close();
 		}
-
+		//Построение блока с блоками{
 		x += dx;
 		for (int i = 0; i<n; i++)
 			if (FloatRect(x + 3, y + 3, 6, 6).intersects(block[i].getGlobalBounds()))
@@ -54,16 +54,16 @@ int main()
 			{
 				block[i].setPosition(-100, 0); dy = -dy;
 			}
-
+		//}
 		if (x<0 || x>520)  dx = -dx;
 		if (y<0 || y>450)  dy = -dy;
 
-		if (Keyboard::isKeyPressed(Keyboard::Right)) sPaddle.move(6, 0);
+		if (Keyboard::isKeyPressed(Keyboard::Right)) sPaddle.move(6, 0);//Реагирование на кнопки клавиатуры
 		if (Keyboard::isKeyPressed(Keyboard::Left)) sPaddle.move(-6, 0);
 
 		if (FloatRect(x, y, 12, 12).intersects(sPaddle.getGlobalBounds())) dy = -(rand() % 5 + 2);
 
-		sBall.setPosition(x, y);
+		sBall.setPosition(x, y);//Сохранение позиции шарика
 
 		app.clear();
 		app.draw(sBackground);
