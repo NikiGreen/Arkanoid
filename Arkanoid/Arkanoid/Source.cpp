1c8db7c39f8bb23d83b845c8a83253edde12d188
NikiGreen <antimagantimagov@mail.ru>
2018-11-22 03:50:48 +0300

    Ver 3.1

--- Arkanoid/Arkanoid/Source.cpp
+++ Arkanoid/Arkanoid/Source.cpp
@@ -5,19 +5,24 @@
 using namespace sf;
 
 void menu(RenderWindow & window) {
-	Texture menuTexture1, menuTexture2, menuTexture3, aboutTexture, menuBackground;
-	menuTexture1.loadFromFile("images/111.png");
-	menuTexture2.loadFromFile("images/222.png");
-	menuTexture3.loadFromFile("images/333.png");
-	aboutTexture.loadFromFile("images/about.png");
-	menuBackground.loadFromFile("images/Penguins.jpg");
-	Sprite menu1(menuTexture1), menu2(menuTexture2), menu3(menuTexture3), about(aboutTexture), menuBg(menuBackground);
+	Font first_font;//шрифт 
+	Texture menuTitleTexture,menuTexture1, menuTexture2, menuTexture3, aboutTexture, menuBackground;
+	menuTitleTexture.loadFromFile("images/Title3.png");
+	menuTexture1.loadFromFile("images/Button_PlayGame.png");
+	menuTexture2.loadFromFile("images/Button_About.png");
+	menuTexture3.loadFromFile("images/Button_Exit.png");
+	aboutTexture.loadFromFile("images/ABOUT2.png");
+	menuBackground.loadFromFile("images/background.jpg");
+	Sprite menuTitle(menuTitleTexture),menu1(menuTexture1), menu2(menuTexture2), menu3(menuTexture3), about(aboutTexture), menuBg(menuBackground);
 	bool isMenu = 1;
 	int menuNum = 0;
-	menu1.setPosition(100, 30);
-	menu2.setPosition(100, 90);
-	menu3.setPosition(100, 150);
-	menuBg.setPosition(345, 0);
+
+	menuTitle.setPosition(-40, -150);
+	menu1.setPosition(/*100*/150, /*30*/190);
+	menu2.setPosition(150, /*90*/250);
+	menu3.setPosition(150, /*150*/310);
+	menuBg.setPosition(0, 0);
+
 
 	//////////////////////////////МЕНЮ///////////////////
 	while (isMenu)
@@ -28,9 +33,9 @@ void menu(RenderWindow & window) {
 		menuNum = 0;
 		window.clear(Color(129, 181, 221));
 
-		if (IntRect(100, 30, 300, 50).contains(Mouse::getPosition(window))) { menu1.setColor(Color::Blue); menuNum = 1; }
-		if (IntRect(100, 90, 300, 50).contains(Mouse::getPosition(window))) { menu2.setColor(Color::Blue); menuNum = 2; }
-		if (IntRect(100, 150, 300, 50).contains(Mouse::getPosition(window))) { menu3.setColor(Color::Blue); menuNum = 3; }
+		if (IntRect(/*100*/150, /*30*/190, 300, 50).contains(Mouse::getPosition(window))) { menu1.setColor(Color::Blue); menuNum = 1; }
+		if (IntRect(/*100*/150, /*90*/250, 300, 50).contains(Mouse::getPosition(window))) { menu2.setColor(Color::Blue); menuNum = 2; }
+		if (IntRect(/*100*/150, /*150*/310, 300, 50).contains(Mouse::getPosition(window))) { menu3.setColor(Color::Blue); menuNum = 3; }
 
 		if (Mouse::isButtonPressed(Mouse::Left))
 		{
@@ -41,6 +46,7 @@ void menu(RenderWindow & window) {
 		}
 
 		window.draw(menuBg);
+		window.draw(menuTitle);
 		window.draw(menu1);
 		window.draw(menu2);
 		window.draw(menu3);
@@ -49,6 +55,38 @@ void menu(RenderWindow & window) {
 	}
 	////////////////////////////////////////////////////
 }
+
+
+void MainAlgorythm(RenderWindow & app,Text text,int Lives,int x,int dx,int y,int dy,Sprite block[],int n) {
+	//text.setString("Lives:" + std::to_string(Lives));//задает строку тексту
+	//Event e;
+	//контроль столкновения блоков и шара{
+	//x += dx;
+	//for (int i = 0; i<n; i++)
+	//	if (FloatRect(x + 3, y + 3, 6, 6).intersects(block[i].getGlobalBounds()))
+	//	{
+	//		block[i].setPosition(-100, 0); dx = -dx;
+	//	}
+
+	//y += dy;
+	//for (int i = 0; i<n; i++)
+	//	if (FloatRect(x + 3, y + 3, 6, 6).intersects(block[i].getGlobalBounds()))
+	//	{
+	//		block[i].setPosition(-100, 0); dy = -dy;
+	//	}
+	//}
+	//if (x<0 || x>520)  dx = -dx;//Контроль стенок
+	//if (y < 0 /*|| y>450*/) {
+
+	//	dy = -dy;
+	//}
+	//else if (y>450) {//Сделать поражение
+	//	Lives--;
+	//	dy = -dy;
+	//}
+}
+
+
 
 int main()
 {
@@ -94,8 +132,20 @@ int main()
 	float dx = 6, dy = 5;//старт шарика
 	float x = 260, y = 440;
 
-	while (app.isOpen() && !Keyboard::isKeyPressed(Keyboard::Escape))//При открытии окна
+	while (app.isOpen())//При открытии окна
 	{
+		if (Lives < 4 ) {//исправить 4 на 1   поражение!!!
+			menu(app);
+			Lives = 5;
+		}
+		else if (Scoure>=100) {//Победа!!!
+			menu(app);
+		}
+		else if (Keyboard::isKeyPressed(Keyboard::Escape)) {//Выход!!!
+			menu(app);
+		}
+
+
 		text.setString("Lives:" + std::to_string(Lives));//задает строку тексту
 		Event e;
 		while (app.pollEvent(e))//Для ловли ивента
@@ -108,14 +158,14 @@ int main()
 		for (int i = 0; i<n; i++)
 			if (FloatRect(x + 3, y + 3, 6, 6).intersects(block[i].getGlobalBounds()))
 			{
-				block[i].setPosition(-100, 0); dx = -dx;
+				block[i].setPosition(-100, 0); dx = -dx; Scoure++;
 			}
 
 		y += dy;
 		for (int i = 0; i<n; i++)
 			if (FloatRect(x + 3, y + 3, 6, 6).intersects(block[i].getGlobalBounds()))
 			{
-				block[i].setPosition(-100, 0); dy = -dy;
+				block[i].setPosition(-100, 0); dy = -dy; Scoure++;
 			}
 		//}
 		if (x<0 || x>520)  dx = -dx;//Контроль стенок
@@ -123,11 +173,11 @@ int main()
 			
 			dy = -dy;
 		}
-		else if (y>450) {
+		else if (y>450) {//Сделать поражение
 			Lives--;
 			dy = -dy;
 			}
-
+		
 		if (Keyboard::isKeyPressed(Keyboard::Right)) sPaddle.move(6, 0);//Реагирование на кнопки клавиатуры
 		if (Keyboard::isKeyPressed(Keyboard::Left)) sPaddle.move(-6, 0);
 
