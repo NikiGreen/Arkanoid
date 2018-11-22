52ba4b2732255a1c302a0565e32096da1cdd11c6
NikiGreen <antimagantimagov@mail.ru>
2018-11-22 09:33:23 +0300

    Ver 5.6

--- Arkanoid/Arkanoid/Menu.h
+++ Arkanoid/Arkanoid/Menu.h
@@ -3,25 +3,29 @@
 #ifndef  _menu_H_
 #define _menu_H_
 #include"1.h"
-
+int cout = 0;
+bool new1 = false;
 //#include <SFML/Graphics.hpp>
 //#include <time.h>
 //#include <iostream>
 //using namespace sf;
  void menu(RenderWindow & window) {
+	 
 	Font first_font;//шрифт 
-	Texture menuTitleTexture, menuTexture1, menuTexture2, menuTexture3, aboutTexture, menuBackground;
+	Texture menuTitleTexture, menuTexture0, menuTexture1, menuTexture2, menuTexture3, aboutTexture, menuBackground;
 	menuTitleTexture.loadFromFile("images/Title3.png");
+	menuTexture0.loadFromFile("images/Button_Resume.png");
 	menuTexture1.loadFromFile("images/Button_PlayGame.png");
 	menuTexture2.loadFromFile("images/Button_About.png");
 	menuTexture3.loadFromFile("images/Button_Exit.png");
 	aboutTexture.loadFromFile("images/ABOUT2.png");
 	menuBackground.loadFromFile("images/background.jpg");
-	Sprite menuTitle(menuTitleTexture), menu1(menuTexture1), menu2(menuTexture2), menu3(menuTexture3), about(aboutTexture), menuBg(menuBackground);
+	Sprite menuTitle(menuTitleTexture), menu0(menuTexture0), menu1(menuTexture1), menu2(menuTexture2), menu3(menuTexture3), about(aboutTexture), menuBg(menuBackground);
 	bool isMenu = 1;
 	int menuNum = 0;
 
 	menuTitle.setPosition(-40, -150);
+	menu0.setPosition(/*100*/150, /*30*/130);
 	menu1.setPosition(/*100*/150, /*30*/190);
 	menu2.setPosition(150, /*90*/250);
 	menu3.setPosition(150, /*150*/310);
@@ -31,11 +35,14 @@
 	//////////////////////////////МЕНЮ///////////////////
 	while (isMenu)
 	{
+		menu0.setColor(Color::White);
 		menu1.setColor(Color::White);
 		menu2.setColor(Color::White);
 		menu3.setColor(Color::White);
 		menuNum = 0;
 		window.clear(Color(129, 181, 221));
+
+		if (IntRect(/*100*/150, /*30*/130, 300, 50).contains(Mouse::getPosition(window))) { menu0.setColor(Color::Blue); menuNum = 0; }
 
 		if (IntRect(/*100*/150, /*30*/190, 300, 50).contains(Mouse::getPosition(window))) { menu1.setColor(Color::Blue); menuNum = 1; }
 		if (IntRect(/*100*/150, /*90*/250, 300, 50).contains(Mouse::getPosition(window))) { menu2.setColor(Color::Blue); menuNum = 2; }
@@ -43,9 +50,17 @@
 
 		if (Mouse::isButtonPressed(Mouse::Left))
 		{
+			if (menuNum == 0){
+				if (cout != 0) {
+					isMenu = false;
+				}
+				
+					 
+			}
+
 			if (menuNum == 1) {
 				isMenu = false;
-				
+				cout++;
 
 			}//если нажали первую кнопку, то выходим из меню 
 			if (menuNum == 2) { window.draw(about); window.display(); while (!Keyboard::isKeyPressed(Keyboard::Escape)); }
@@ -55,6 +70,7 @@
 
 		window.draw(menuBg);
 		window.draw(menuTitle);
+		window.draw(menu0);
 		window.draw(menu1);
 		window.draw(menu2);
 		window.draw(menu3);
